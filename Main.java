109d4c348e7add9670bc452bfc13f6cd69aa8b3e
Arish <arish.wahab15@gmail.com>
2022-05-07 18:03:40 +1000

    changes 7 may

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World");
+  System.out.println("Hello World");
+  System.out.println("I am learning Java.");
+  System.out.println("YOU SHOULD TRY IT!");
+  System.out.println("SEE YA");
+  System.out.println(3+3);
+  System.out.println(3+3);
+  System.out.println(3+3);
+  System.out.println(3+3);
+  System.out.println(3+3);
+  System.out.println(3+3);
+  System.out.println(3+3);
+  System.out.print("Hello killer! ");
+  System.out.print("I will kill you instantly!");
+
+
   }
 }
